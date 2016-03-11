5a9dfe00dfc2c4cd837df4b723133f0e57bc5565
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-03-11 17:10:20 -0600

    Add extra signup information controller

---
+++ Snappy/SignUpInfoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignUpInfoViewController.swift
+//  Snappy
+//
+//  Created by Steven on 3/11/16.
+//  Copyright © 2016 SwiftSyndicate. All rights reserved.
+//
+
+import UIKit
+
+class SignUpInfoViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
