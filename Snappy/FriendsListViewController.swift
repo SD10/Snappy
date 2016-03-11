8d3991274fd853aa9f1c3df10baf5205409bfa3c
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-03-10 20:00:14 -0600

    Friends list initial UI

--- Snappy/FriendsListViewController.swift
+++ Snappy/FriendsListViewController.swift
@@ -13,14 +13,16 @@ class FriendsListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+
+    @IBAction func cameraButtonPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
