27e0bff2d30f95110bc869cea5f90ca26cf3383c
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-03-21 23:17:52 -0500

    Refactor login controller

--- Snappy/LoginViewController.swift
+++ Snappy/LoginViewController.swift
@@ -28,29 +28,20 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     var player = AVAudioPlayer()
     
     // MARK: - View Life Cycle
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Prepare Camera Sound Effect
-        let audioPath = NSBundle.mainBundle().pathForResource("cameraShutter", ofType: "wav")!
-        
-        do {
-            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
-        } catch let error as NSError {
-            print(error.debugDescription)
-        }
         
         //Set UITextField Delegates
         emailTextField.delegate = self
         passwordTextField.delegate = self
         
         //Format UIElements
-        loginBackground.alpha = 0.72
-        setAlphaZero()
+        setInitialAlphas()
         formatButton(loginButton)
         formatButton(facebookButton)
         formatButton(signupButton)
+        
+        prepareCameraSound()
         let _ = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "flashEffect", userInfo: nil, repeats: false)
     }
 
@@ -70,13 +61,112 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         button.layer.cornerRadius = 12.0
     }
     
-    // Set Alpha of UIElements to zero
-    func setAlphaZero() {
+    // Set initial alpha values of UIElements
+    func setInitialAlphas() {
         loginButton.alpha = 0
         signupButton.alpha = 0
         facebookButton.alpha = 0
         emailTextField.alpha = 0
         passwordTextField.alpha = 0
+        loginBackground.alpha = 0.72
+    }
+    
+    // MARK: - Login In / Sign Up
+    
+    // Create and Display Error Alert
+    func showErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func checkTextFields() -> Bool {
+        if !emailTextField.text!.isEmpty && !passwordTextField.text!.isEmpty {
+            return true
+        } else {
+            showErrorAlert("Empty text field", message: "Please fill out all fields to continue")
+            return false
+        }
+    }
+    
+    func logUserIn() {
+        if checkTextFields() {
+            DataService.dataService.REF_USERS.authUser(emailTextField.text!, password: passwordTextField.text!, withCompletionBlock: { error, authData in
+                if error != nil {
+                    switch error.code {
+                    case STATUS_EMAIL_INVALID:
+                        self.showErrorAlert("Invalid Email", message: "Please enter a valid email")
+                    case STATUS_ACCOUNT_NONEXIST:
+                        self.showErrorAlert("Email or Password Invalid", message: "Please enter a valid email and password")
+                    default: break
+                    }
+                } else {
+                    NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: KEY_UID)
+                }
+            })
+        }
+    }
+
+    // Sign in with Facebook
+    @IBAction func facebookPressed(sender: UIButton!) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["email"], fromViewController: nil) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            
+            if facebookError != nil {
+                print("Facebook login failed. Error \(facebookError)")
+            } else if facebookResult.isCancelled {
+                self.dismissViewControllerAnimated(true, completion: nil)
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfully logged in with facebook. \(accessToken)")
+                
+                DataService.dataService.REF_USERS.authWithOAuthProvider("facebook", token: accessToken, withCompletionBlock: { error, authData in
+                    if error != nil {
+                        print("Login failed. \(error)")
+                    } else {
+                        print("Logged In! \(authData)")
+                        //FIXME: - Fix force unwrap provider
+                        let user = ["provider": authData.provider!, "displayName": authData.providerData["displayName"]!, "email": authData.providerData["email"]!, "profileImage": authData.providerData["profileImageURL"]!]
+                        DataService.dataService.createFirebaseUser(authData.uid, user: user)
+                        NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: KEY_UID)
+                        self.delegate?.didLoginSuccessfully()
+                    }
+                })
+            }
+        }
+    }
+    
+    // Email-Password Login Attempt
+    @IBAction func attemptLogin(sender: UIButton) {
+        logUserIn()
+        self.delegate?.didLoginSuccessfully()
+    }
+    
+    
+    // Sign Up for New Account
+    @IBAction func attemptSignUp(sender: UIButton) {
+        if checkTextFields() {
+            DataService.dataService.REF_USERS.createUser(emailTextField.text!, password: passwordTextField.text!, withValueCompletionBlock: { error, result in
+                if error != nil {
+                    self.showErrorAlert("Could not create account", message: "Try something else?")
+                } else {
+                    self.logUserIn()
+                    self.performSegueWithIdentifier("addInformation", sender: nil)
+                }
+            })
+        }
+    }
+
+    
+    // MARK: - Keyboard Management
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
     }
     
     // MARK: - Animations
@@ -106,122 +196,20 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             aView.alpha = 0.0
             self.player.play()
             self.loginBackground.alpha = 0.72
-
+            
             }, completion: { (done) -> Void in
                 aView.removeFromSuperview()
         })
     }
     
-    // MARK: - Login In / Sign Up
-
-    // Sign in with Facebook
-    @IBAction func facebookPressed(sender: UIButton!) {
-        let facebookLogin = FBSDKLoginManager()
-        facebookLogin.logInWithReadPermissions(["email"], fromViewController: nil) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
-            
-            if facebookError != nil {
-                print("Facebook login failed. Error \(facebookError)")
-            } else if facebookResult.isCancelled {
-                self.dismissViewControllerAnimated(true, completion: nil)
-            } else {
-                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
-                print("Successfully logged in with facebook. \(accessToken)")
-                
-                DataService.dataService.REF_USERS.authWithOAuthProvider("facebook", token: accessToken, withCompletionBlock: { error, authData in
-                    if error != nil {
-                        print("Login failed. \(error)")
-                    } else {
-                        print("Logged In! \(authData)")
-                        /* This is very bad but would need more error handling to avoid this force unwrap.
-                             Create a firebase user */
-                        let user = ["provider": authData.provider!, "displayName": authData.providerData["displayName"]!, "email": authData.providerData["email"]!, "profileImage": authData.providerData["profileImageURL"]!]
-                        DataService.dataService.createFirebaseUser(authData.uid, user: user)
-                        NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: KEY_UID)
-                        self.delegate?.didLoginSuccessfully()
-                    }
-                })
-            }
+    // MARK: - Sound Effects
+    func prepareCameraSound() {
+        let audioPath = NSBundle.mainBundle().pathForResource("cameraShutter", ofType: "wav")!
+        
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+        } catch let error as NSError {
+            print(error.debugDescription)
         }
     }
-    
-    // Email-Password Login Attempt
-    @IBAction func attemptLogin(sender: UIButton) {
-        if let email = emailTextField.text where email != "", let pwd = passwordTextField.text where pwd != "" {
-            DataService.dataService.REF_USERS.authUser(email, password: pwd, withCompletionBlock: { error, authData in
-                if error != nil {
-                    switch error.code {
-                        case STATUS_EMAIL_INVALID:
-                            self.showErrorAlert("Invalid Email", message: "Please enter a valid email")
-                        case STATUS_ACCOUNT_NONEXIST:
-                            self.showErrorAlert("Email or Password Invalid", message: "Please enter a valid email and password")
-                        default: break
-                    }
-                } else {
-                    NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: KEY_UID)
-                    self.delegate?.didLoginSuccessfully()
-                }
-            })
-        } else {
-            showErrorAlert("Email and Password Required", message: "Please enter a valid email and password to continue")
-        }
-    }
-    
-    
-    // Sign Up for New Account
-    @IBAction func attemptSignUp(sender: UIButton) {
-        if let email = emailTextField.text where email != "", let pwd = passwordTextField.text where pwd != "" {
-            
-            // Account aready exists, then log them in
-            DataService.dataService.REF_USERS.authUser(email, password: pwd, withCompletionBlock: { error, authData in
-                if error != nil {
-                    switch error.code {
-                        //Handle Invalid Email
-                        case STATUS_EMAIL_INVALID:
-                            self.showErrorAlert("Invalid Email", message: "Please enter a valid email to sign up")
-                        case STATUS_ACCOUNT_NONEXIST:
-                            DataService.dataService.REF_USERS.createUser(email, password: pwd, withValueCompletionBlock: { error, result in
-                                if error != nil {
-                                    self.showErrorAlert("Could not create account", message: "Try something else?")
-                                } else {
-                                    // Log In User After Creating Account
-                                    self.signupButton.enabled = false
-                                    NSUserDefaults.standardUserDefaults().setValue(result[KEY_UID], forKey: KEY_UID)
-                                    DataService.dataService.REF_USERS.authUser(email, password: pwd, withCompletionBlock: { error, authData in
-                                        let user = ["provider": authData.provider!, "password": "\(pwd)", "email": "\(email)"]
-                                        DataService.dataService.createFirebaseUser(authData.uid, user: user)
-                                        self.performSegueWithIdentifier("addInformation", sender: nil)
-                                    })
-                                }
-                            })
-                        default: break
-                    }
-                } else {
-                    // No error so log the user in
-                    self.delegate?.didLoginSuccessfully()
-                }
-            })
-        }
-    }
-    
-    // Create and Display Error Alert
-    func showErrorAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
-        let action = UIAlertAction(title: "Ok", style: .Default, handler: nil)
-        alert.addAction(action)
-        presentViewController(alert, animated: true, completion: nil)
-    }
-    
-    // MARK: - Keyboard Management
-
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        view.endEditing(true)
-    }
-    
-    
-
 }
